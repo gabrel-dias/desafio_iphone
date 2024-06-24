86a4e29e1e49c575b850286a4398ee32a78127d4
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-24 10:44:23 -0300

    anotações inseridas

--- src/aplicativos/Navegador.java
+++ src/aplicativos/Navegador.java
@@ -6,8 +6,11 @@ import java.net.URI;
 public class Navegador {
     public void abrirPagina() {
         try {
-            URI uri = new URI("https://linktr.ee/tecdias");
-            Desktop.getDesktop().browse(uri);
+            URI uri = new URI("https://linktr.ee/tecdias"); // criação de um URI que será utilizado como URL,
+            // é preciso fazer um tratamento de exceção, pois é lançada a URISyntaxException
+
+            Desktop.getDesktop().browse(uri); // o método browse abre o navegador padrão da máquina do usuário na url
+            // que foi previamente inserida
         } catch (Exception e) {
             e.printStackTrace();
         }
