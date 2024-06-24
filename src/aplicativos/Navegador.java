69774baee3affea49f773b876bbaff442524b47d
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-23 21:22:15 -0300

    remoção de string desnecessária

--- src/aplicativos/Navegador.java
+++ src/aplicativos/Navegador.java
@@ -6,7 +6,6 @@ import java.net.URI;
 public class Navegador {
     public void abrirPagina() {
         try {
-//            String url = "https://linktr.ee/tecdias";
             URI uri = new URI("https://linktr.ee/tecdias");
             Desktop.getDesktop().browse(uri);
         } catch (Exception e) {
